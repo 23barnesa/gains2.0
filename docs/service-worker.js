783f04678bd88745ca54623cf809945d6e45ef75
Alec <95661173+23barnesa@users.noreply.github.com>
2026-09-09 08:20:38 -0400

    Create service-worker.js

---
+++ docs/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "gainlog-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
