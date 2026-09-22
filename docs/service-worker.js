99a31de7c5bdbb2844cbbea458caa295c35d9cb6
Alec <95661173+23barnesa@users.noreply.github.com>
2026-09-22 11:52:09 -0400

    Refresh GainLog PWA cache

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "gainlog-v2.3.0";
-const SHELL = ["./","./index.html","./styles.css?v=2.3.0","./app.js?v=2.3.0","./manifest.webmanifest"];
+const CACHE = "gainlog-v2.5.0";
+const SHELL = ["./","./index.html","./styles.css?v=2.5.0","./app.js?v=2.5.0","./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
